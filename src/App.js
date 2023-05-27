6a9a85e0be9d629279e37977883f07dc2564597a
theofficial-dp <61872467+theofficial-dp@users.noreply.github.com>
2023-05-27 12:34:04 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,103 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function TravelForm() {
+  const [vacationLength, setVacationLength] = useState('');
+  const [expectedBudget, setExpectedBudget] = useState('');
+  const [selfDescription, setSelfDescription] = useState('');
+  const [preferredLocation, setPreferredLocation] = useState('');
+  const [travelDate, setTravelDate] = useState('');
+  const [specificRequests, setSpecificRequests] = useState('');
+
+  const handleVacationLengthChange = (event) => {
+    setVacationLength(event.target.value);
+  };
+
+  const handleExpectedBudgetChange = (event) => {
+    setExpectedBudget(event.target.value);
+  };
+
+  const handleSelfDescriptionChange = (event) => {
+    setSelfDescription(event.target.value);
+  };
+
+  const handlePreferredLocationChange = (event) => {
+    setPreferredLocation(event.target.value);
+  };
+
+  const handleTravelDateChange = (event) => {
+    setTravelDate(event.target.value);
+  };
+
+  const handleSpecificRequestsChange = (event) => {
+    setSpecificRequests(event.target.value);
+  };
+
+  const handleDecideForMeClick = () => {
+    // Add logic to handle the button click, e.g., display recommendations based on the inputs
+    console.log('You clicked "You decide for me"');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h2>Travel Planner</h2>
+      <form>
+        <label>
+          How long vacation are you planning?
+          <input
+            type="text"
+            value={vacationLength}
+            onChange={handleVacationLengthChange}
+          />
+        </label>
+        <br />
+        <label>
+          How much do you expect to spend?
+          <input
+            type="text"
+            value={expectedBudget}
+            onChange={handleExpectedBudgetChange}
+          />
+        </label>
+        <br />
+        <label>
+          How do you describe yourself?
+          <input
+            type="text"
+            value={selfDescription}
+            onChange={handleSelfDescriptionChange}
+          />
+        </label>
+        <br />
+        <label>
+          Do you have a preferred location in mind?
+          <input
+            type="text"
+            value={preferredLocation}
+            onChange={handlePreferredLocationChange}
+          />
+        </label>
+        <br />
+        <label>
+          When are you expecting to travel?
+          <input
+            type="text"
+            value={travelDate}
+            onChange={handleTravelDateChange}
+          />
+        </label>
+        <br />
+        <label>
+          Anything specific you would like us to take care of?
+          <textarea
+            value={specificRequests}
+            onChange={handleSpecificRequestsChange}
+          />
+        </label>
+        <br />
+        <button onClick={handleDecideForMeClick}>You decide for me</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default TravelForm;
